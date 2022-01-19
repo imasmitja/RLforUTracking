d8f1b827dbee5e25ba3f5475fdd0da1c8524e3f8
imasmitja <masmitja@icm.csic.es>
2022-01-19 14:12:47 -0800

    add folders

---
+++ multiagent/core.py
@@ -0,0 +1,235 @@
+import numpy as np
+
+# physical/external base state of all entites
+class EntityState(object):
+    def __init__(self):
+        # physical position
+        self.p_pos = None
+        # physical velocity
+        self.p_vel = None
+        self.p_vel_old = None
+
+# state of agents (including communication and internal/mental state)
+class AgentState(EntityState):
+    def __init__(self):
+        super(AgentState, self).__init__()
+        # communication utterance
+        self.c = None
+
+# action of the agent
+class Action(object):
+    def __init__(self):
+        # physical action
+        self.u = None
+        # communication action
+        self.c = None
+
+# properties and state of physical world entity
+class Entity(object):
+    def __init__(self):
+        # name 
+        self.name = ''
+        # properties:
+        self.size = 0.050
+        # entity can move / be pushed
+        self.movable = False
+        # entity collides with others
+        self.collide = True
+        # material density (affects mass)
+        self.density = 25.0
+        # color
+        self.color = None
+        # max speed and accel
+        self.max_speed = None
+        self.accel = None
+        # state
+        self.state = EntityState()
+        # mass
+        self.initial_mass = 1.0
+
+    @property
+    def mass(self):
+        return self.initial_mass
+
+# properties of landmark entities
+class Landmark(Entity):
+     def __init__(self):
+        super(Landmark, self).__init__()
+        # action
+        self.action = Action()
+        # physical motor noise amount
+        self.u_noise = None
+
+# properties of agent entities
+class Agent(Entity):
+    def __init__(self):
+        super(Agent, self).__init__()
+        # agents are movable by default
+        self.movable = True
+        # cannot send communication signals
+        self.silent = False
+        # cannot observe the world
+        self.blind = False
+        # physical motor noise amount
+        self.u_noise = None
+        # communication noise amount
+        self.c_noise = None
+        # control range
+        self.u_range = 1.0
+        # state
+        self.state = AgentState()
+        # action
+        self.action = Action()
+        # script behavior to execute
+        self.action_callback = None
+
+# multi-agent world
+class World(object):
+    def __init__(self):
+        # list of agents and entities (can change at execution-time!)
+        self.agents = []
+        self.landmarks = []
+        self.landmarks_estimated = []
+        # communication channel dimensionality
+        self.dim_c = 0
+        # position dimensionality
+        self.dim_p = 2
+        # color dimensionality
+        self.dim_color = 3
+        # simulation timestep
+        self.dt = 0.1
+        # physical damping
+        self.damping = 0.25
+        # contact response parameters
+        self.contact_force = 1e+2
+        self.contact_margin = 1e-3
+        # if world is collaborative
+        self.num_agents = 3
+        self.num_landmarks = 3
+        self.collaborative = True
+        self.angle = []
+
+    # return all entities in the world
+    @property
+    def entities(self):
+        return self.agents + self.landmarks
+
+    # return all agents controllable by external policies
+    @property
+    def policy_agents(self):
+        return [agent for agent in self.agents if agent.action_callback is None]
+
+    # return all agents controlled by world scripts
+    @property
+    def scripted_agents(self):
+        return [agent for agent in self.agents if agent.action_callback is not None]
+
+    # update state of the world
+    def step(self):
+        # set actions for scripted agents 
+        for agent in self.scripted_agents:
+            agent.action = agent.action_callback(agent, self)
+        # gather forces applied to entities
+        p_force = [None] * len(self.entities)
+        # apply agent physical controls
+        p_force = self.apply_action_force(p_force)
+        # apply environment forces
+        p_force = self.apply_environment_force(p_force)
+        # integrate physical state
+        self.integrate_state(p_force)
+        # update agent state
+        for agent in self.agents:
+            self.update_agent_state(agent)
+
+    # gather agent action forces
+    def apply_action_force(self, p_force):
+        # set applied forces
+        for i,entity in enumerate(self.entities):
+            if 'agent' in entity.name:
+                if entity.movable:
+                    noise = np.random.randn(*entity.action.u.shape) * entity.u_noise if entity.u_noise else 0.0
+                    p_force[i] = entity.action.u + noise 
+            if 'landmark' in entity.name:
+                if entity.movable:
+                    noise = np.random.randn(*entity.action.u.shape) * entity.u_noise if entity.u_noise else 0.0
+                    p_force[i] = entity.action.u + noise 
+        return p_force
+
+    # gather physical forces acting on entities
+    def apply_environment_force(self, p_force):
+        # simple (but inefficient) collision response
+        for a,entity_a in enumerate(self.entities):
+            for b,entity_b in enumerate(self.entities):
+                if(b <= a): continue
+                [f_a, f_b] = self.get_collision_force(entity_a, entity_b)
+                if(f_a is not None):
+                    if(p_force[a] is None): p_force[a] = 0.0
+                    p_force[a] = f_a + p_force[a] 
+                if(f_b is not None):
+                    if(p_force[b] is None): p_force[b] = 0.0
+                    p_force[b] = f_b + p_force[b]        
+        return p_force
+
+    # integrate physical state
+    def integrate_state(self, p_force):
+        for i,entity in enumerate(self.entities):
+            if not entity.movable: continue
+            entity.state.p_vel = entity.state.p_vel * (1 - self.damping)
+            
+            if 'landmark' in entity.name:
+                #if entity is a landmark (x-y force applyied independently)
+                if (p_force[i] is not None):
+                    entity.state.p_vel += (p_force[i] / entity.mass) * self.dt
+                if entity.max_speed is not None:
+                    speed = np.sqrt(np.square(entity.state.p_vel[0]) + np.square(entity.state.p_vel[1]))
+                    if speed > entity.max_speed:
+                        entity.state.p_vel = entity.state.p_vel / np.sqrt(np.square(entity.state.p_vel[0]) +
+                                                                      np.square(entity.state.p_vel[1])) * entity.max_speed
+                entity.state.p_pos += entity.state.p_vel * self.dt
+            
+            if 'agent' in entity.name:
+                #if entity is an agnet (constant velocity, increment of angle)
+                '''This is the new approach designed by Ivan'''
+                #First position of p_vel is the angular velocity which is used to increase the angle of the agent
+                if (p_force[i] is not None):
+                    # entity.state.p_vel[0] = p_force[i] * 0.1 #multiply by 0.1 to set radius limit at 100m minimum (taken into consideration that the p_force are bounded between -1 and 1)
+                    self.angle[i] += p_force[i].item(0)*0.3 #multiply by 0.1 to set radius limit at 100m minimum
+                    if self.angle[i] > np.pi*2.:
+                        self.angle[i] -= np.pi*2.
+                    if self.angle[i] < -np.pi*2:
+                        self.angle[i] += np.pi*2
+                #The second position of p_vel is the liniar velocity of the agent
+                # vel = entity.state.p_vel[1]+0.
+                vel = 0.3 #seting this velocity (0.1) and considering that the dt is equal to 0.1, means that we have a new position each 10m.
+                if vel < 0:
+                    vel = 0
+                #Finally, we increase the position (aka the agent) using the new angle and velocity.
+                entity.state.p_pos += np.array([vel*np.cos(self.angle[i]),vel*np.sin(self.angle[i])]) * self.dt
+                entity.state.p_vel = np.array([vel*np.cos(self.angle[i]),vel*np.sin(self.angle[i])])               
+                
+    def update_agent_state(self, agent):
+        # set communication state (directly for now)
+        if agent.silent:
+            agent.state.c = np.zeros(self.dim_c)
+        else:
+            noise = np.random.randn(*agent.action.c.shape) * agent.c_noise if agent.c_noise else 0.0
+            agent.state.c = agent.action.c + noise      
+
+    # get collision forces for any contact between two entities
+    def get_collision_force(self, entity_a, entity_b):
+        if (not entity_a.collide) or (not entity_b.collide):
+            return [None, None] # not a collider
+        if (entity_a is entity_b):
+            return [None, None] # don't collide against itself
+        # compute actual distance between entities
+        delta_pos = entity_a.state.p_pos - entity_b.state.p_pos
+        dist = np.sqrt(np.sum(np.square(delta_pos)))
+        # minimum allowable distance
+        dist_min = entity_a.size + entity_b.size
+        # softmax penetration
+        k = self.contact_margin
+        penetration = np.logaddexp(0, -(dist - dist_min)/k)*k
+        force = self.contact_force * delta_pos / dist * penetration
+        force_a = +force if entity_a.movable else None
+        force_b = -force if entity_b.movable else None
+        return [force_a, force_b]
---
+++ multiagent/scenarios/simple_track_ivan_6.py
@@ -0,0 +1,280 @@
+import numpy as np
+from multiagent.core import World, Agent, Landmark
+from multiagent.scenario import BaseScenario
+from target_pf import Target
+from utilities import random_levy
+
+
+class Scenario(BaseScenario):
+    
+    def make_world(self, num_agents=3, num_landmarks=3, landmark_depth=15., landmark_movable = False, movement='linear', pf_method = False, rew_err_th=0.0003, rew_dis_th=0.3):
+        world = World()
+        # set any world properties first
+        world.dim_c = 2
+        world.num_agents = num_agents
+        world.num_landmarks = num_landmarks
+        world.collaborative = True
+        # add agents
+        world.agents = [Agent() for i in range(num_agents)]
+        for i, agent in enumerate(world.agents):
+            agent.name = 'agent %d' % i
+            agent.collide = True
+            agent.silent = True
+            agent.size = 0.04
+        # add landmarks
+        world.landmarks = [Landmark() for i in range(num_landmarks*2)]
+        for i, landmark in enumerate(world.landmarks):
+            if i < num_landmarks:
+                landmark.name = 'landmark %d' % i
+                landmark.collide = False
+                landmark.movable = landmark_movable
+            else:
+                landmark.name = 'landmark_estimation %d' % (i-num_landmarks)
+                landmark.collide = False
+                landmark.movable = False
+                landmark.size = 0.002
+                
+        # make initial conditions
+        world.cov = np.ones(num_landmarks)
+        world.error = np.ones(num_landmarks)
+        self.reset_world(world)
+        
+        #benchmark variables
+        self.agent_outofworld = 0
+        self.landmark_collision = 0
+        self.agent_collision = 0
+        self.landmark_depth = landmark_depth
+        self.movement = movement
+        self.pf_method = pf_method
+        self.rew_err_th = rew_err_th
+        self.rew_dis_th = rew_dis_th
+        return world
+
+    def reset_world(self, world):
+        # random properties for agents
+        for i, agent in enumerate(world.agents):
+            agent.color = np.array([0.35, 0.35, 0.85])
+        # random properties for landmarks
+        for i, landmark in enumerate(world.landmarks):
+            if i < world.num_landmarks:
+                landmark.color = np.array([0.25, 0.25, 0.25])
+            else:
+                landmark.color = np.array([0.55, 0.0, 0.0])
+        # set random initial states
+        for agent in world.agents:
+            agent.state.p_pos = np.random.uniform(-1., 1., world.dim_p)
+            agent.state.p_vel = np.zeros(world.dim_p)
+            agent.state.p_vel_old = np.zeros(world.dim_p)
+            agent.state.c = np.zeros(world.dim_c)
+        for i, landmark in enumerate(world.landmarks):
+            if i < world.num_landmarks:
+                dis = np.random.uniform(0.04, 1.)
+                rad = np.random.uniform(0, np.pi*2)
+                landmark.state.p_pos = agent.state.p_pos + np.array([np.cos(rad),np.sin(rad)])*dis
+                landmark.state.p_vel = np.zeros(world.dim_p)
+            else:
+                landmark.state.p_pos = world.agents[0].state.p_pos
+                landmark.state.p_vel = np.zeros(world.dim_p)
+        #Initailize the landmark estimated positions
+        world.landmarks_estimated = [Target() for i in range(world.num_landmarks)]
+        
+        #benchmark variables
+        self.agent_outofworld = 0
+        self.landmark_collision = 0
+        self.agent_collision = 0
+        
+
+    def benchmark_data(self, agent, world):
+        landmarks_real_p = []
+        for i in range(world.num_landmarks):
+            landmarks_real_p.append(world.landmarks[i].state.p_pos)
+        # return (rew, collisions, min_dists, occupied_landmarks,landmarks_real_p)
+        return(world.error,landmarks_real_p, self.agent_outofworld, self.landmark_collision, self.agent_collision)
+
+
+    def is_collision(self, agent1, agent2):
+        delta_pos = agent1.state.p_pos - agent2.state.p_pos
+        dist = np.sqrt(np.sum(np.square(delta_pos)))
+        dist_min = agent1.size + agent2.size
+        return True if dist < dist_min else False
+    
+    done_state = False
+    def reward(self, agent, world):
+        global done_state
+        done_state = False
+        # Agents are rewarded based on landmarks_estimated covariance_vals, penalized for collisions
+        rew = 0.
+        
+        for i,l in enumerate(world.landmarks_estimated):
+            # reward as a function of covariance matrix (PF)
+            world.cov[i] = np.sqrt((l.pf.covariance_vals[0])**2+(l.pf.covariance_vals[1])**2)
+            # rew -= world.cov[i]/100
+            # reward as a function of the distance error between the landmark and its estimation (PF or LS)
+            if self.pf_method == True:
+                world.error[i] = np.sqrt((l.pfxs[0]-world.landmarks[i].state.p_pos[0])**2+(l.pfxs[2]-world.landmarks[i].state.p_pos[1])**2) #Error from PF
+            else:
+                world.error[i] = np.sqrt((l.lsxs[-1][0]-world.landmarks[i].state.p_pos[0])**2+(l.lsxs[-1][2]-world.landmarks[i].state.p_pos[1])**2) #Error from LS
+            
+            # rew -= 0.01*(world.error[i])**2
+            # rew += 0.01*(0.004-world.error[i])
+            
+            if world.error[i]<self.rew_err_th:
+                rew += 1.
+            else:
+                rew += 0.01*(0.004-world.error[i])
+        
+        dists = [np.sqrt(np.sum(np.square(agent.state.p_pos - l.state.p_pos))) for l in world.landmarks[:-world.num_landmarks]]
+        
+        #For Test 11
+        
+        for dist in dists:
+            # rew += 10*np.exp(-1/2*(dist-0.1)**2/0.1)-5
+            # rew += 0.01*(np.exp(-10*(0.2-dist)**2)-0.9) #test 123
+            
+            # rew -= 0.01*(0.1-dist)**2  #test 121 and 122
+            
+            if dist < self.rew_dis_th: #other tests
+                rew += 1.
+                # rew += 0.1*(0.5-0.1)
+            else:
+                rew += 0.01*(0.7-dist)
+                
+        if min(dists) > 1.5: #agent outside the world
+            rew -= 100
+            done_state = True
+            self.agent_outofworld += 1
+        if min(dists) < 0.02: #is collision
+            rew -= 1.
+            done_state = True
+            self.landmark_collision += 1
+            
+            
+        # reward based on increment of action (from paper ieeeAccess) done in test 25      
+        
+        #compute the angle between the old direction and the new direction 
+        # rew -= 0.0001*abs(agent.state.p_vel.item(0))
+        
+        #old methods
+        # inc_action = agent.state.p_vel_old - agent.state.p_vel
+        # rew -= 0.001*np.sqrt(inc_action[0]**2+inc_action[1]**2)
+        # agent.state.p_vel_old = agent.state.p_vel + 0.
+        # if np.all(np.sign(agent.state.p_vel_old) == np.sign(agent.state.p_vel)) == True:
+        #     rew = 0.01
+            
+            
+        
+            
+        if agent.collide:
+            for a in world.agents:
+                if a is agent: continue
+                if self.is_collision(a, agent):
+                    rew -= 10.
+                    self.agent_collision += 1
+                    done_state = True
+        return rew
+
+    def observation(self, agent, world):
+        # get positions of all entities in this agent's reference frame
+        entity_pos = []
+        entity_range = []
+        for i, entity in enumerate(world.landmarks):
+            if i < world.num_landmarks: 
+                #Update the landmarks_estiamted position using Particle Fileter
+                #1:Compute radius between the agent and each landmark
+                slant_range = np.sqrt(((entity.state.p_pos - agent.state.p_pos)[0])**2+((entity.state.p_pos - agent.state.p_pos)[1])**2)
+                target_depth = self.landmark_depth/1000. #normalize the target depth
+                slant_range = np.sqrt(slant_range**2+target_depth**2) #add target depth to the range measurement
+                # Add some systematic error in the measured range
+                slant_range *= 1.01 # where 0.99 = 1% of sound speed difference = 1495 m/s
+                # Add some noise in the measured range
+                slant_range += np.random.uniform(-0.001, +0.001)
+                # Return to a planar range
+                slant_range = np.sqrt(abs(slant_range**2-target_depth**2))
+                #2:Update the PF
+                if self.pf_method == True:
+                    world.landmarks_estimated[i].updatePF(dt=0.04, new_range=True, z=slant_range, myobserver=[agent.state.p_pos[0],0.,agent.state.p_pos[1],0.], update=True)
+                else:
+                    #2b: Update the LS
+                    world.landmarks_estimated[i].updateLS(dt=0.04, new_range=True, z=slant_range, myobserver=[agent.state.p_pos[0],0.,agent.state.p_pos[1],0.])
+                # Traditional plot
+                # import matplotlib.pyplot as plt
+                # plt.figure(figsize=(5,5))
+                # plt.plot(world.landmarks_estimated[i].pf._x[0],world.landmarks_estimated[i].pf._x[2], 'r^', ms=20)
+                # plt.plot(world.landmarks_estimated[i].pf.x.T[0],world.landmarks_estimated[i].pf.x.T[2], 'ro', ms=5, alpha=0.3)
+                # plt.plot(world.landmarks[0].state.p_pos[0],world.landmarks[0].state.p_pos[1], 'ko', ms=6, alpha = 0.5)
+                # plt.plot(world.agents[0].state.p_pos[0],world.agents[0].state.p_pos[1], 'bo', ms=6, alpha = 0.5)
+                # plt.xlim(-1,1)
+                # plt.ylim(-1,1)
+                # plt.show()
+                #3:Publish the new estimated position
+                if self.pf_method == True:
+                    world.landmarks[i+world.num_landmarks].state.p_pos = [world.landmarks_estimated[i].pfxs[0],world.landmarks_estimated[i].pfxs[2]] #Using PF
+                else:
+                    world.landmarks[i+world.num_landmarks].state.p_pos = [world.landmarks_estimated[i].lsxs[-1][0],world.landmarks_estimated[i].lsxs[-1][2]] #Using LS
+                
+                #Append the position of the landmark to generate the observation state
+                #Using the true landmark position
+                # entity_pos.append(entity.state.p_pos - agent.state.p_pos)
+                #Using the estimated landmark position
+                entity_pos.append(world.landmarks[i+world.num_landmarks].state.p_pos - agent.state.p_pos)
+                #Using the estimated landmark position but without delating the agent position. so it has a global position.
+                # entity_pos.append(world.landmarks[i+world.num_landmarks].state.p_pos)
+                entity_range.append(slant_range)
+                
+                # Move the landmark if movable
+                if entity.movable:
+                    if self.movement == 'linear':
+                        #linear movement
+                        u_force = 0.05
+                        entity.action.u = np.array([np.cos(self.ra)*u_force,np.sin(self.ra)*u_force])
+                    
+                    elif self.movement == 'random':
+                        # random movement
+                        entity.action.u = np.random.randn(2)/2.
+                    
+                    elif self.movement == 'levy':
+                        #random walk Levy movement
+                        beta = 1.9 #must be between 1 and 2
+                        entity.action.u = random_levy(beta)
+                        if entity.state.p_pos[0] > 0.8:
+                            entity.action.u[0] = -abs(entity.action.u[0])
+                        if entity.state.p_pos[0] < -0.8:
+                            entity.action.u[0] = abs(entity.action.u[0])
+                        if entity.state.p_pos[1] > 0.8:
+                            entity.action.u[1] = -abs(entity.action.u[1])
+                        if entity.state.p_pos[1] < -0.8:
+                            entity.action.u[1] = abs(entity.action.u[1])
+                    
+                        
+                        
+                        
+                
+        # entity colors
+        entity_color = []
+        for entity in world.landmarks:  # world.entities:
+            entity_color.append(entity.color)
+        # communication of all other agents
+        comm = []
+        other_pos = []
+        for other in world.agents:
+            if other is agent: continue
+            comm.append(other.state.c)
+            other_pos.append(other.state.p_pos - agent.state.p_pos)
+        # return np.concatenate([agent.state.p_vel] + [agent.state.p_pos] + entity_pos + other_pos + comm)
+        # return np.concatenate([agent.state.p_vel] + [agent.state.p_pos] + entity_pos + other_pos)
+        
+        #observations used until test 40
+        return np.concatenate([agent.state.p_vel] + [agent.state.p_pos] + entity_pos + other_pos + [entity_range])
+        #observations used on test 41 and test 114
+        # return np.concatenate([agent.state.p_vel] + [agent.state.p_pos] + other_pos + [entity_range])
+    
+    def done(self, agent, world):
+        # episodes are done based on the agents minimum distance from a landmark.
+        global done_state
+        # dists = [np.sqrt(np.sum(np.square(agent.state.p_pos - l.state.p_pos))) for l in world.landmarks[:-world.num_landmarks]]
+        # if min(dists) > 1.5 or min(dists) < 0.1:
+        if done_state:
+            done = True
+        else:
+            done = False
+        return done
